cb2f33c093dc0962dbb1ec556ba54c85264c84f4
nikola-bozin-org <nikola.bozin.org@gmail.com>
2024-04-27 22:57:23 +0200

    feat: add initial loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use tokio::{task::JoinHandle, time};
+
+pub struct PgDbIdleAgent{
+    interval_secs:Duration
+}
+
+impl PgDbIdleAgent{
+    pub fn new(interval_secs:Duration)->Self{
+        Self {
+            interval_secs
+        }
+    }
+
+    pub fn start(&self)->JoinHandle<()>{
+        let mut ticker = time::interval(self.interval_secs);
+        tokio::task::spawn(async move {
+            loop {
+                ticker.tick().await;
+                println!("Hello World")
+            }
+        })
+    }
+}
+
+
+#[tokio::main]
+async fn main() {
+    let x = PgDbIdleAgent::new(Duration::from_secs(1));
+    let handle = x.start();
+    handle.await.unwrap() // Await the handle to prevent the main function from exiting prematurely
 }
